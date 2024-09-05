8177a3b22a94899bc18ddfa3618b1b0d0b51c426
princeemensah <pmensah@aimsammi.org>
2024-09-05 00:49:47 +0000

    added data visualization and updated script

--- iris_pipeline.py
+++ iris_pipeline.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import seaborn as sns
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
 def load_dataset():
     data = load_iris()
@@ -13,7 +15,7 @@ def load_dataset():
     return df
 
 def train(df):
-    X_train, X_test, y_train, y_test = train_test_split(df.iloc[:, :-2], df['species'], test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(df.iloc[:, :-1], df['species'], test_size=0.2, random_state=42)
     model = LogisticRegression()
     model.fit(X_train, y_train)
 
@@ -25,9 +27,47 @@ def get_accuracy(model, X_test, y_test):
 
     return accurcy
 
+def plot_feature(df, feature):
+    # Plot a histogram of one of the features
+    df[feature].hist()
+    plt.title(f"Distribution of {feature}")
+    plt.xlabel(feature)
+    plt.ylabel("Frequency")
+    plt.show()
+
+def plot_features(df):
+    # Plot scatter plot of first two features.
+    scatter = plt.scatter(
+        df["sepal length (cm)"], df["sepal width (cm)"], c=df["species"]
+    )
+    plt.title("Scatter plot of the sepal features (width vs length)")
+    plt.xlabel(xlabel="sepal length (cm)")
+    plt.ylabel(ylabel="sepal width (cm)")
+    plt.legend(
+        scatter.legend_elements()[0],
+        df["species_names"].unique(),
+        loc="lower right",
+        title="Classes",
+    )
+    plt.show()
+
+def plot_model(model, X_test, y_test):
+    # Plot the confusion matrix for the model
+    ConfusionMatrixDisplay.from_estimator(estimator=model, X=X_test, y=y_test)
+    plt.title("Confusion Matrix")
+    plt.show()
+
+
+
 if __name__ == "__main__":
     data = load_dataset()
     print(data.head())
     model, X_train, X_test, y_train, y_test = train(data)
     accurcy = get_accuracy(model, X_test, y_test)
     print(f"Accuracy: {accurcy:.2f}")
+
+    plot_feature(data, "sepal length (cm)")
+    plot_features(data)
+    plot_model(model, X_test, y_test)
+
+    
